51b50217c1d5bc8468b5fbbf8f60101defe85dc3
Naw7k <n_m_k0@hotmail.com>
2026-04-13 07:06:49 +0300

    Add files via upload

---
+++ src/main/java/net/naw/shut_down/mixin/PauseScreenAccessor.java
@@ -0,0 +1,11 @@
+package net.naw.shut_down.mixin;
+
+import net.minecraft.client.gui.screens.PauseScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PauseScreen.class)
+public interface PauseScreenAccessor {
+    @Accessor("showPauseMenu")
+    boolean isShowPauseMenu();
+}
